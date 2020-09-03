2e74b2efe8ae199f821a24be385af3363d99bcd6
dsattenlop <dsattenlop@haverford.edu>
2020-09-03 15:27:42 -0400

    9/3 draft

--- catalog/views.py
+++ catalog/views.py
@@ -62,7 +62,6 @@ class PersonListView(generic.ListView):
 class PersonDetailView(generic.DetailView):
     model = Person
 
-
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
